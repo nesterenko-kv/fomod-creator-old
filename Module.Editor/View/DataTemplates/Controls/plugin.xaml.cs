ea4f23d8d3f9b8e12992b822219265a605e67c4d
nestquik <nestquik@gmail.com>
2016-04-29 20:17:57 +0300

    Улучшения во View'ах.

--- Module.Editor/View/DataTemplates/Controls/plugin.xaml.cs
+++ Module.Editor/View/DataTemplates/Controls/plugin.xaml.cs
@@ -1,24 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Module.Editor.View.DataTemplates.Controls
+﻿namespace Module.Editor.View.DataTemplates.Controls
 {
-    /// <summary>
-    /// Логика взаимодействия для plugin.xaml
-    /// </summary>
-    public partial class plugin : UserControl
+    public partial class plugin
     {
         public plugin()
         {
