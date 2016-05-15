56ee14acdefc0d7e750e2f15f5507afe2e8600ea
amarf <glebovae@inbox.ru>
2016-05-15 22:45:26 +0300

    Исправил ошибки байдингов Добавил умный селектор для Шелл

---
+++ Application/Selectors/TabItemTemplateSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MainApplication.Selectors
+{
+    public class TabItemTemplateSelector : StyleSelector
+    {
+        public Style DefaultStyle { get; set; }
+
+        public ObservableCollection<SelectorItem> TemplateList { get; set; } = new ObservableCollection<SelectorItem>();
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            if (item == null)
+                return DefaultStyle;
+
+            Type findType;
+            var fe = item as FrameworkElement;
+            if (fe != null)
+                findType = fe.DataContext.GetType();
+            else
+                findType = item.GetType();
+
+
+            var t = TemplateList.FirstOrDefault(i => i.DataType == findType);
+            if (t != null) return t.Style;
+
+            return DefaultStyle;
+        }
+    }
+
+
+
+    public class SelectorItem
+    {
+        public Type DataType { get; set; }
+        public Style Style { get; set; }
+    }
+}
